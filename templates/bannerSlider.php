b482b628c7299e6b5e99858e1c0b27144a10c2e7
Jorge Villalobos <JVS.Jorge@gmail.com>
2015-01-28 23:11:10 -0600

    Banner Slider

---
+++ templates/bannerSlider.php
@@ -0,0 +1,22 @@
+<!-- Wrapper for slides -->
+<div id="carousel-banner" class="carousel slide" data-ride="carousel">
+ 
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="/wp-content/uploads/Images/Slider1.png">
+    </div>
+    <div class="item">
+      <img src="/wp-content/uploads/Images/Slider2.png">
+    </div>
+  </div>
+  <div class="carousel-content">
+      <p class="center">ENJOY THE PARADISE</p>
+      <h1 class="center">WELCOME TO COSTA RICA</h1>
+      <p><span class="center">GET STARTED</span></p>
+  </div>
+ 
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-banner" role="button" data-slide="prev"><span>‹</span></a>
+  <a class="right carousel-control" href="#carousel-banner" role="button" data-slide="next"><span >›</span></a>
+</div> <!-- Carousel -->
