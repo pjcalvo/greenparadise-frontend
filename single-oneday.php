752ec9edb7bc12f58bb21be45536c3b167dc8c1b
Jorge Villalobos <JVS.Jorge@gmail.com>
2015-04-08 22:36:23 -0600

    One Day Single restructor firstPart

--- single-oneday.php
+++ single-oneday.php
@@ -4,20 +4,50 @@
 ?>
 
 <div class="single">
-    <div class="header singleOneDayHeader small-banner center">
-        <div class="img-header-content center">
-            <h1 class="title t1 white text-center">ONE <span class="white"> DAY TOUR</span></h1>
-        </div>
-    </div>
     <div class="text-center singleHeader">
-            <h1 class="title t2 dark-green"><?php echo get_the_title() ?></h1>
+            <h1 class="title t4 text-center dark-green">ONE DAY <span> TOUR</span></h1>
             <p class="text t-paragraph text-center"><?php echo get_the_excerpt() ?></p>
     </div>
+    <hr>
     <div class="content-medium">
         <div class="text-center">
             <img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id()) ?>">
         </div>
-        <label><h2>Price:    </h2> </label><span class="subtitle">    $<?php echo get_field("oneday_price"); ?></span>
-        <div class="text t-paragraph text-justify"><?php echo the_field("description"); ?></div>
+        <div class="col-sm-12">
+            <h1 class="title t4_1"><?php echo get_the_title() ?></h1>
+            <p class"text t-paragraph"><?php echo get_the_content() ?></p>
+        </div>
+        <div class="col-sm-12">
+            <div class="col-sm-4">
+                <h1 class="title t4_2 dark-green">AVALIABLE DATES:</h1>
+                <br>
+                <h1 class="title t5">AVALIABLE DATES:</h1>
+                <p class="text t-paragraph">Del 16 al 19 de Abril</p>
+                <p class="text t-paragraph">Del 16 al 19 de Abril</p>
+                <p class="text t-paragraph">Del 16 al 19 de Abril</p>
+            </div>
+            <div class="col-sm-4">
+                <h1 class="title t4_2 dark-green">PACKAGE DETAILS:</h1>
+                <br>
+                <h1 class="title t5">INCLUDES:</h1>
+                <p class="text t-paragraph">Boletos aéreos</p>
+                <p class="text t-paragraph">Vuelo directo </p>
+                <p class="text t-paragraph">Traslados Hotel/Destino/Hotel</p>
+            </div>
+            <div class="col-sm-4">
+                <h1 class="title t4_2 dark-green">PRICE:</h1>
+                <br>
+                </label>
+                <h1 class="title t1_1 blue-cerulean"><span>$</span><?php echo get_field("oneday_price"); ?>
+                    <sup class="t12">PER PERSON</sup>
+                </h1>
+                <button type="button" class="btn btn-green">BUY THIS TOUR</button>
+            </div>
+        </div>
+        <div class="col-sm-12">
+            <h1 class="title t4_3">MORE DESCRIPTION</h1>
+            <div class="text t-paragraph text-justify"><?php echo the_field("description"); ?></div>
+        </div>
     </div>
 </div>
+<?php get_template_part('templates/destinations', 'page'); ?>
